add5ae923f15b8a7e27446507b0c1cf30809511d
ERP604 <118367429+ERP604@users.noreply.github.com>
2025-01-29 11:52:38 -0600

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/pengu/Downloads/MathLib-main/MathLib-main
-# Build directory: C:/Users/pengu/Downloads/MathLib-main/MathLib-main/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[AllTests]=] "C:/Users/pengu/Downloads/MathLib-main/MathLib-main/cmake-build-debug/test_suite.exe")
-set_tests_properties([=[AllTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/pengu/Downloads/MathLib-main/MathLib-main/CMakeLists.txt;43;add_test;C:/Users/pengu/Downloads/MathLib-main/MathLib-main/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
--- cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/pengu/Downloads/MathLib-main/MathLib-main/cmake-build-debug/_deps/googletest-src
-# Build directory: C:/Users/pengu/Downloads/MathLib-main/MathLib-main/cmake-build-debug/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
